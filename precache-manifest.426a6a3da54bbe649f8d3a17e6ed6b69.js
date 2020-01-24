948cd18ec9250011f296d6607c1e6bdeef4b8eca
Abhishek Apte <abhishek.aapte@gmail.com>
2020-01-24 21:21:13 +0530

    Updates

---
+++ precache-manifest.426a6a3da54bbe649f8d3a17e6ed6b69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f865a0f73afa34525debf2b34f5567",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7e946ca6813d84f226d",
+    "url": "/static/js/2.6f31cc19.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.6f31cc19.chunk.js.LICENSE"
+  },
+  {
+    "revision": "095b1a9189fbab93ada2",
+    "url": "/static/js/main.c69a844b.chunk.js"
+  },
+  {
+    "revision": "ce0a157cde10015b1a85",
+    "url": "/static/js/runtime-main.9f8fe1ca.js"
+  }
+]);
